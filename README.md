8ffecd226c74c32328a7f3754d94afebae342e13
jasalazar <jsal01@msn.com>
2017-05-17 15:58:26 -0400

    My first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Pipes
+
+This project was made to implement pipes for the first time.
---
+++ src/app/app.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  nombre = "Jose";
+
+  arreglo = [1,2,3,4,5,6,7,8,9,10];
+
+  PI = Math.PI;
+
+  a = 0.234;
+
+  salario = 1234.5;
+
+  heroe = {
+    nombre: "Logan",
+    clave: "Wolverine",
+    edad: 500,
+    direccion: {
+      calle: "Primera",
+      casa: "19"
+    }
+  };
+
+  valorDePromesa = new Promise( ( resolve, reject )=> {
+
+    setTimeout( ()=> resolve( 'Llego la data!'), 3500 );
+
+  });
+
+  fecha = new Date();
+
+  nombre2 = "Jose arNAldo salazar useche";
+
+  video = "TZaQzSAjczI";
+
+  activar:boolean = true;
+
+}
